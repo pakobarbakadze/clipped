bf9da2432cfa5ea2d4825d22090aee94e069cc66
pakobarbakadze <pakobarbakadze@gmail.com>
2024-06-18 11:41:49 +0400

    refactor: api global prefix

--- src/main.ts
+++ src/main.ts
@@ -17,9 +17,12 @@ async function bootstrap() {
     .setDescription('The API description')
     .setVersion('1.0')
     .addTag('api')
+    .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('api/docs', app, document);
+
+  app.setGlobalPrefix('api');
 
   await app.listen(PORT);
 }
